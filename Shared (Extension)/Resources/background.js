4abfd49e7bc055c5073caebda881d8578a22c797
Scott <scott0228@email.com>
2022-03-14 07:24:20 +0800

    測試 sendNativeMessage

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -3,6 +3,10 @@ const converter = OpenCC.Converter({ from: 'cn', to: 'tw' });
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // console.log("Received request: ", request.text);
     let result = converter(request.text);
-    // console.log("result: ", result);
-    sendResponse(result);
+    // await
+    browser.runtime.sendNativeMessage("application.id", {text: request.text}, function(response) {
+        console.log("Received sendNativeMessage response: ", response);
+        // console.log("result: ", result);
+        // sendResponse(response);
+    });
 });
--- Shared (Extension)/SafariWebExtensionHandler.swift
+++ Shared (Extension)/SafariWebExtensionHandler.swift
@@ -7,12 +7,22 @@
 
 import SafariServices
 import os.log
+import OpenCC
 
 let SFExtensionMessageKey = "message"
 
 class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
 
     func beginRequest(with context: NSExtensionContext) {
+        
+        var str = "鼠标里面的硅二极管坏了，导致光标分辨率降低。1"
+        let converter = try! ChineseConverter(options: [.traditionalize, .twStandard, .twIdiom])
+        str = converter.convert(str)
+        print(str)
+        let converter1 = try! ChineseConverter(options: [.simplify, .twStandard, .twIdiom])
+        str = converter1.convert(str)
+        print(str)
+        
         let item = context.inputItems[0] as! NSExtensionItem
         let message = item.userInfo?[SFExtensionMessageKey]
         os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
