653531b4c2372a32c87303a8e1ada696abae978f
Scott <scott0228@email.com>
2022-03-13 08:39:15 +0800

    改 icon

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,13 @@
 console.log("Hello World!", browser);
+
+document.querySelectorAll('label').forEach(item => {
+  item.addEventListener('click', event => {
+      console.log(document.querySelector('input[name="transferType"]:checked').value);
+  })
+})
+
+document.querySelectorAll('input').forEach(item => {
+  item.addEventListener('change', event => {
+      console.log('AAA: ' + event.srcElement.value);
+  })
+})
